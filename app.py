9e17ec99ac484b98d56d88799cf807d7b77dbf60
Théophyl <th.theophyl.hoguet@gmail.com>
2021-09-15 17:11:08 +0200

    ajout des pages html

--- app.py
+++ app.py
@@ -2,10 +2,20 @@ from  flask import *
 from markupsafe import escape
 
 app = Flask(__name__)
+app.secret_key = b"***REMOVED***"
 
 @app.route("/")
-def main():
+def index():
     return render_template("index.html")
+
+@app.route("/ts")
+def ts():
+    return render_template("ts.html")
+
+@app.route("/twitch")
+def twitch():
+    return render_template("twitch.html")
+
 @app.route("/<name>")
 def hello(name):
     return f"Hello, {escape(name)}!"
